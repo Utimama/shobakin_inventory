7a38fb5775b250ec631029d918b1268b1c75d5a4
Utimama <agbasulai@yahoo.com>
2024-03-04 18:34:06 +0100

    '#' update

---
+++ main_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from main_app import views
+
+urlpatterns = [
+    path("dash/", views.index, name="dash"),
+    path("products/", views.products, name="products"),
+    path("orders/", views.orders, name="orders"),
+    path("users/", views.users, name="users"),
+    path("user/", views.user, name="user"),
+    path("register/", views.register, name="register"),
+]
